217fc1e17da09974464ad3c6bd4da7f0dbcb183a
Alex Sandro <alexsrs@gmail.com>
2025-04-20 12:06:43 -0300

    core: add CORS

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,20 @@
 import express from "express";
 import router from "./routes";
+import cors from "cors";
 
 function createApp(){
     const app = express();
     app.use(express.json());
     app.use("/api", router);
+
+    const corsOptions = {
+        origin: "*",
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+    };
+    // Enable CORS for all routes and methods
+    app.use(cors(corsOptions));
+
     return app;
 }
 
